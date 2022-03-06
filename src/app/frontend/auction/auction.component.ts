896153b3ef1cfa35f7d47940c691ed4b19a320be
Saqib-Sattar <saqib.sattar@therightsw.com>
2022-03-06 20:41:32 +0500

    convert auction form into reactive form

--- src/app/frontend/auction/auction.component.ts
+++ src/app/frontend/auction/auction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auction',
@@ -7,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuctionComponent implements OnInit {
 
+  form!: FormGroup;
+  showProductDes: any = {};
+  
   constructor() { }
 
   ngOnInit(): void {
+      var emailPattern = "^[a-zA-Z]{1}[a-zA-Z0-9.\-_]*@[a-zA-Z]{1}[a-zA-Z.-]*[a-zA-Z]{1}[.][a-zA-Z]{2,}$";
+
+      this.form = new FormGroup({
+        address : new FormControl('', [Validators.required]),
+        email: new FormControl('',    [Validators.required,
+                                        Validators.email,
+                                        Validators.pattern(emailPattern)]),
+        xrpBid: new FormControl('', [Validators.required]),
+        fakBid: new FormControl('', [Validators.required])
+      })
   }
+  // convenience getter for easy access to Registration fields
+  get f() { return this.form.controls; }
+  
+  auctionForm() {
+
+  }
+
+
   show: boolean = false;
   clickEvent(){
     this.show = true;       
