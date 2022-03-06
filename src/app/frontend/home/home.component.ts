db56eea0e6440b50126f397a66e4537ce9e89329
Saqib-Sattar <saqib.sattar@therightsw.com>
2022-03-06 18:13:30 +0500

    convert contactUs form into reactive form

--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  form!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    var namePattern = '^(?! )[A-Za-z ]*(?<! )$';
+    var messagePattern = "^[a-zA-Z].*[\s\.]*$";
+    var emailPattern = "^[a-zA-Z]{1}[a-zA-Z0-9.\-_]*@[a-zA-Z]{1}[a-zA-Z.-]*[a-zA-Z]{1}[.][a-zA-Z]{2,}$";
+
+    this.form = new FormGroup({
+      name : new FormControl('', [Validators.required,
+                                  Validators.pattern(namePattern),
+                                  Validators.minLength(3)]),
+      email: new FormControl('',    [Validators.required,
+                                      Validators.email,
+                                      Validators.pattern(emailPattern)]),
+      message: new FormControl('', [Validators.required,
+                                    Validators.pattern(messagePattern),
+                                    Validators.minLength(3)])
+    })
   }
+
+  // convenience getter for easy access to contactUs fields
+  get f() { return this.form.controls; }
+
+  contactUs() {
+    console.log(this.form.value)
+  }
+
   show: boolean = false;
   clickEvent(){
     this.show = !this.show;       
